1feefebc70085e8c8a46ee59d205a44ac091f1ab
Jiaxiang Li <alex.lijiaxiang@foxmail.com>
2018-12-28 19:38:26 +0800

    1. update test.R

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,7 @@
+### 2018-12-28 19:38:12
+
+1. update test.R
+
 ### 2018-12-28 13:44:06
 
 1. add r proj and readme and news
---
+++ test.R
@@ -0,0 +1,13 @@
+library(RODBC)
+impala <- odbcConnect("Impala")
+sqlQuery(impala,"
+insert into opd.t181228_schedule_task_test_ljx
+select 
+    1 as x1
+    -- ,'测试中文' as chi
+    -- ,'测试正则化' as repex
+    ,current_timestamp() as inserttime
+
+         ")
+
+g
